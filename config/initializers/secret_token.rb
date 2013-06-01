a329278303ad8a1f02d250bf53851fec8e8c80fc
thepolycube <polycube@live.com>
2013-05-31 22:19:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asuka::Application.config.secret_token = '0fb7b3fcb30fb9176121510a22aa67ca9a0441497c6ca51a578311038a4856ab70887b7209f362a531609ef577f76e5c1da68b1d6d6d506ad9497fdd75a8ea98'
